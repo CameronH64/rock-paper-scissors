396aa8b6c0773bdda537aece22c3aa7308a615eb
CameronH64 <cholbrook582@gmail.com>
2023-04-23 20:19:15 -0500

    Preliminary server gui working, and starts server

--- rock_paper_scissors_server.py
+++ rock_paper_scissors_server.py
@@ -4,56 +4,58 @@
 import socket
 import threading
 
-# -------------------------- SERVER-SIDE --------------------------
-
-def handle_client(connection, address):
-    print(f"[SERVER] NEW CONNECTION: {address} CONNECTED.")
-
-    connected = True
-    while connected:
-        message_length = connection.recv(HEADER).decode(FORMAT)
-        if message_length:
-            message_length = int(message_length)
-            msg = connection.recv(message_length).decode(FORMAT)
-
-            if msg == DISCONNECT_MESSAGE:       # If the server receives the disconnect message, disconnect the client.
-                connected = False
-
-            print(f"[{address}] {msg}")         # Debugging
-            connection.send("Message received.".encode(FORMAT))
-
-    connection.close()
-
-
-# -------------------------- SERVER SETUP --------------------------
-
-def start_server():
-    server.listen()
-    print(f"[LISTENING] SERVER IS LISTENING ON: {SERVER}")
-
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-
-
-
-
-if __name__ == '__main__':
-
+class Server:
     # -------------------------- SERVER-SIDE SETUP --------------------------
+    def __init__(self):
 
-    HEADER = 64
-    PORT = 5050
-    SERVER = socket.gethostbyname(
-        socket.gethostname())  # Will get the IP Address of this computer. If a VPN is used, will use that IP address instead.
-    ADDR = (SERVER, PORT)
-    FORMAT = 'utf-8'
-    DISCONNECT_MESSAGE = "DISCONNECT"
+        self.HEADER = 64
+        self.PORT = 5050
+        self.SERVER = socket.gethostbyname(socket.gethostname())  # Will get the IP Address of this computer. If a VPN is used, will use that IP address instead.
+        self.ADDR = (self.SERVER, self.PORT)
+        self.FORMAT = 'utf-8'
+        self.DISCONNECT_MESSAGE = "DISCONNECT"
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.ADDR)
 
-    print("[STARTING] server is starting...")
-    start_server()
+        # print("[STARTING] server is starting...")
+        # start_server()
+
+
+
+    # -------------------------- SERVER-SIDE --------------------------
+
+    # def setup_server(self):
+
+
+
+    def handle_client(self, connection, address):
+        print(f"[SERVER] NEW CONNECTION: {address} CONNECTED.")
+
+        connected = True
+        while connected:
+            message_length = connection.recv(self.HEADER).decode(self.FORMAT)
+            if message_length:
+                message_length = int(message_length)
+                msg = connection.recv(message_length).decode(self.FORMAT)
+
+                if msg == self.DISCONNECT_MESSAGE:       # If the server receives the disconnect message, disconnect the client.
+                    connected = False
+
+                print(f"[{address}] {msg}")         # Debugging
+                connection.send("Message received.".encode(self.FORMAT))
+
+        connection.close()
+
+
+    # -------------------------- SERVER SETUP --------------------------
+
+    def start_server(self):
+        self.server.listen()
+        print(f"[LISTENING] SERVER IS LISTENING ON: {self.SERVER}")
+
+        while True:
+            conn, addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
