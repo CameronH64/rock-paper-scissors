00b6d524984dc014ea10916001642d0a27bdbc68
CameronH64 <cholbrook582@gmail.com>
2023-04-25 22:39:55 -0500

    Back to just code implementation.

--- rock_paper_scissors_client.py
+++ rock_paper_scissors_client.py
@@ -3,40 +3,75 @@
 
 import socket
 
-class Client:
+
+# -------------------------- CLIENT-SIDE SETUP --------------------------
 
 
-    # -------------------------- CLIENT-SIDE SETUP --------------------------
+# Set basic variables.
+HEADER = 64                             # Header size
+PORT = 5050                             # Port number to use the socket on.
+FORMAT = 'utf-8'                        # Format to encode data in.
+DISCONNECT_MESSAGE = "DISCONNECT"                           # The message needed to disconnect from the server.
 
-    def __init__(self):
+address = input('Enter an IP address: ')
 
-        # Set basic variables.
-        self.HEADER = 64                             # Header size
-        self.PORT = 5050                             # Port number to use the socket on.
-        self.FORMAT = 'utf-8'                        # Format to encode data in.
-        self.DISCONNECT_MESSAGE = "DISCONNECT"                           # The message needed to disconnect from the server.
 
-        # self.SERVER = input('Enter the server\'s IP address: ')          # Empty for now because I'm not  for now. Needs to contain the IP Address of the server.
-        self.ADDR = None
-        self.client = None
+# self.SERVER = input('Enter the server\'s IP address: ')          # Empty for now because I'm not  for now. Needs to contain the IP Address of the server.
+ADDR = (address, PORT)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
-    # -------------------------- CLIENT-SIDE --------------------------
 
-    def setup_client(self, ip_address):
-        self.ADDR = (ip_address, self.PORT)
+# -------------------------- CLIENT-SIDE --------------------------
 
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect(self.ADDR)
+def setup_client(ip_address, port_number):
+    connection_information = (ip_address, port_number)
 
-    # Send a message to the server.
-    def send(self, message):
-        message = message.encode(self.FORMAT)                        # Encode the string in utf-8 format.
-        msg_length = len(message)                           # Get the message length
-        send_length = str(msg_length).encode(self.FORMAT)        # Make the send length using the msg length and the encode format.
-        send_length += b' ' * (self.HEADER - len(send_length))   # Then, add a buffer to the rest of the send length.
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(connection_information)
 
-        self.client.send(send_length)                            # Send to the server the send_length.
-        self.client.send(message)                                # Send to the server the actual message.
 
-        print(self.client.recv(2048).decode(self.FORMAT))
+# Send a message to the server.
+def send(message):
+    message = message.encode(FORMAT)                        # Encode the string in utf-8 format.
+    msg_length = len(message)                           # Get the message length
+    send_length = str(msg_length).encode(FORMAT)        # Make the send length using the msg length and the encode format.
+    send_length += b' ' * (HEADER - len(send_length))   # Then, add a buffer to the rest of the send length.
+
+    client.send(send_length)                            # Send to the server the send_length.
+    client.send(message)                                # Send to the server the actual message.
+
+    print(client.recv(2048).decode(FORMAT))
+
+
+def handle_message_from_server():
+    # create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect the socket to the server's address and port
+    server_address = (address, PORT)
+    sock.connect(server_address)
+
+    try:
+        # receive data from the server
+        data = sock.recv(1024)
+        return data.decode('utf-8')
+    finally:
+        # close the socket
+        sock.close()
+
+
+# -------------------- RUNNING CODE --------------------------
+
+setup_client(address, PORT)
+
+game_running = True
+
+while game_running:
+    choice = input('Enter choice: ')
+    send(choice)
+
+    # print(handle_message_from_server())
+
+
 
--- rock_paper_scissors_server.py
+++ rock_paper_scissors_server.py
@@ -4,58 +4,88 @@
 import socket
 import threading
 
-class Server:
-    # -------------------------- SERVER-SIDE SETUP --------------------------
-    def __init__(self):
+# -------------------------- SERVER-SIDE SETUP --------------------------
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())  # Will get the IP Address of this computer. If a VPN is used, will use that IP address instead.
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "exit"
 
-        self.HEADER = 64
-        self.PORT = 5050
-        self.SERVER = socket.gethostbyname(socket.gethostname())  # Will get the IP Address of this computer. If a VPN is used, will use that IP address instead.
-        self.ADDR = (self.SERVER, self.PORT)
-        self.FORMAT = 'utf-8'
-        self.DISCONNECT_MESSAGE = "DISCONNECT"
+client_list = []
 
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(self.ADDR)
-
-        # print("[STARTING] server is starting...")
-        # start_server()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
 
 
-    # -------------------------- SERVER-SIDE --------------------------
+# -------------------------- SERVER-SIDE --------------------------
 
-    # def setup_server(self):
+def handle_client(connection, address):
+    print(f"[SERVER] NEW CONNECTION: {address} CONNECTED.")
 
+    server_running = True
 
+    action_counter = 0
 
-    def handle_client(self, connection, address):
-        print(f"[SERVER] NEW CONNECTION: {address} CONNECTED.")
+    while server_running:
 
-        connected = True
-        while connected:
-            message_length = connection.recv(self.HEADER).decode(self.FORMAT)
-            if message_length:
-                message_length = int(message_length)
-                msg = connection.recv(message_length).decode(self.FORMAT)
+        message_length = connection.recv(HEADER).decode(FORMAT)
 
-                if msg == self.DISCONNECT_MESSAGE:       # If the server receives the disconnect message, disconnect the client.
-                    connected = False
+        if connection not in client_list:
+            client_list.append(connection)
 
-                print(f"[{address}] {msg}")         # Debugging
-                connection.send("Message received.".encode(self.FORMAT))
-
-        connection.close()
+        if message_length:
+            message_length = int(message_length)
+            message = connection.recv(message_length).decode(FORMAT)
 
 
-    # -------------------------- SERVER SETUP --------------------------
 
-    def start_server(self):
-        self.server.listen()
-        print(f"[LISTENING] SERVER IS LISTENING ON: {self.SERVER}")
+            # If statement that will deal with input data
+            if message == DISCONNECT_MESSAGE:       # If the server receives the disconnect message, disconnect the client.
+                server_running = False
+            elif message == 'rock':
+                print('[SERVER] RECEIVED ROCK')
+                connection.send(f"Message received: {message}".encode(FORMAT))
+                action_counter += 1
+            elif message == 'paper':
+                print('[SERVER] RECEIVED PAPER')
+                connection.send(f"Message received: {message}".encode(FORMAT))
+                action_counter += 1
+            elif message == 'scissors':
+                print('[SERVER] RECEIVED SCISSORS')
+                connection.send(f"Message received: {message}".encode(FORMAT))
+                action_counter += 1
 
-        while True:
-            conn, addr = self.server.accept()
-            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
-            thread.start()
-            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+            # After client information is received, check if enough data to make a game judgment.
+            if action_counter % 2 == 0:
+                print('Do rps comparison')
+            elif action_counter % 2 == 1:
+                print('Do NOT do rps comparison')
+
+
+            print(f"[{address}] {message}")         # Debugging
+            connection.send(f"Message received: {message}".encode(FORMAT))
+
+    connection.close()
+
+
+# -------------------------- SERVER SETUP --------------------------
+
+def start_server():
+    server.listen()
+    print(f"[LISTENING] SERVER IS LISTENING ON: {SERVER}")
+
+    while True:
+        conn, addr = server.accept()                                            # Accept any and all new client socket connections.
+        thread = threading.Thread(target=handle_client, args=(conn, addr))      # Instantiate a new thread for the new client.
+
+        thread.start()                                                          # Start that thread.
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")           # Debugging for printing the number of active socket connections.
+
+
+
+# ------------------- RUNNING CODE -----------------------
+
+start_server()
