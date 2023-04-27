45f9fe37c952025c557bfe71eb5556115a537a8f
CameronH64 <cholbrook582@gmail.com>
2023-04-26 20:52:08 -0500

    More stable client connection

--- rock_paper_scissors_client.py
+++ rock_paper_scissors_client.py
@@ -6,91 +6,71 @@ import socket
 import threading
 
 
-# -------------------------- CLIENT-SIDE SETUP --------------------------
-
-
-# Set basic variables.
-HEADER = 64                             # Header size
-PORT = 5050                             # Port number to use the socket on.
-FORMAT = 'utf-8'                        # Format to encode data in.
-DISCONNECT_MESSAGE = "DISCONNECT"                           # The message needed to disconnect from the server.
-
-
-# address = input('Enter an IP address: ')
-
-
-# self.SERVER = input('Enter the server\'s IP address: ')          # Empty for now because I'm not  for now. Needs to contain the IP Address of the server.
-# ADDR = (address, PORT)
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(ADDR)
-
-
 # -------------------------- CLIENT-SIDE --------------------------
 
 def setup_client(ip_address, port_number):
-    connection_information = (ip_address, port_number)
+    connection_information = (ip_address, port_number)              # Create a tuple that has the IP address and port number to connect to the server.
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(connection_information)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # Create the client socket.
+    client.connect(connection_information)                          # Connect this socket with the information above.
 
     return client
 
 # Send a message to the server.
 def send(message, client):
-    message = message.encode(FORMAT)                        # Encode the string in utf-8 format.
-    msg_length = len(message)                           # Get the message length
-    send_length = str(msg_length).encode(FORMAT)        # Make the send length using the msg length and the encode format.
-    send_length += b' ' * (HEADER - len(send_length))   # Then, add a buffer to the rest of the send length.
 
-    client.send(send_length)                            # Send to the server the send_length.
+    message = message.encode('utf-8')                        # Encode the string in utf-8 format.
     client.send(message)                                # Send to the server the actual message.
 
-    print(client.recv(2048).decode(FORMAT))
 
 
-# def handle_message_from_server():
-#     # create a TCP/IP socket
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-#     # connect the socket to the server's address and port
-#     server_address = (address, PORT)
-#     sock.connect(server_address)
-#
-#     try:
-#         # receive data from the servera
-#         data = sock.recv(1024)
-#         return data.decode('utf-8')
-#     finally:
-#         # close the socket
-#         sock.close()
+def handle_message_from_server(address, PORT):
+    # create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect the socket to the server's address and port
+    server_address = (address, PORT)
+    sock.connect(server_address)
+
+    try:
+        # Receive data from the server.
+        data = sock.recv(1024)              # Blocks the code.
+        return data.decode('utf-8')
+    finally:
+        # close the socket
+        sock.close()
 
 
-# -------------------- RUNNING CODE --------------------------
+def start_client_setup_thread(server_address_textfield):
+
+    ip_address = server_address_textfield.get("1.0", 'end-1c')
+
+    client = setup_client(ip_address, 5050)
+    # send('message', client)
+
+
+
+threading.Thread(target=setup_client, args=())
+
 
 
 # ------------------------ CREATE CLIENT GUI STUFF ------------------------
 
+root = Tk()
 
-client = setup_client('10.14.0.2', 5050)
-send('message', client)
+server_address_label = Label(root, text='Server Address')
+server_address_textfield = Text(root, height=1, width=20)
+connect_button = Button(root, text='Connect', command=lambda: start_client_setup_thread(server_address_textfield))
+connection_status = Label(root, text='Disconnected')
 
+# rock_photo = tkinter.PhotoImage(file='assets/rock.jpg')
+# rock_label = Label(root, image=rock_photo)
 
+server_address_label.grid(row=0, column=0)
+server_address_textfield.grid(row=0, column=1)
+connect_button.grid(row=1, column=0)
+connection_status.grid(row=2, column=0)
 
-# root = Tk()
-#
-# server_address_label = Label(root, text='Server Address')
-# server_address_textfield = Text(root, height=1, width=20)
-# connect_button = Button(root, text='Connect')
-# connection_status = Label(root, text='Disconnected')
-#
-# # rock_photo = tkinter.PhotoImage(file='assets/rock.jpg')
-# # rock_label = Label(root, image=rock_photo)
-#
-# server_address_label.grid(row=0, column=0)
-# server_address_textfield.grid(row=0, column=1)
-# connect_button.grid(row=1, column=0)
-# connection_status.grid(row=2, column=0)
-#
-# # rock_label.grid(row=3, column=0)
-#
-# root.mainloop()
+# rock_label.grid(row=3, column=0)
+
+root.mainloop()
