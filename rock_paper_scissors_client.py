20e76fad95ea0f83b7bba9f2d7fcec25721ce5d1
CameronH64 <cholbrook582@gmail.com>
2023-04-26 20:17:39 -0500

    PROGRESS

--- rock_paper_scissors_client.py
+++ rock_paper_scissors_client.py
@@ -1,7 +1,9 @@
 # Cameron Holbrook
 # UCA ID: B01257833
-
+import tkinter
+from tkinter import *
 import socket
+import threading
 
 
 # -------------------------- CLIENT-SIDE SETUP --------------------------
@@ -13,13 +15,14 @@ PORT = 5050                             # Port number to use the socket on.
 FORMAT = 'utf-8'                        # Format to encode data in.
 DISCONNECT_MESSAGE = "DISCONNECT"                           # The message needed to disconnect from the server.
 
-address = input('Enter an IP address: ')
+
+# address = input('Enter an IP address: ')
 
 
 # self.SERVER = input('Enter the server\'s IP address: ')          # Empty for now because I'm not  for now. Needs to contain the IP Address of the server.
-ADDR = (address, PORT)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+# ADDR = (address, PORT)
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(ADDR)
 
 
 # -------------------------- CLIENT-SIDE --------------------------
@@ -30,9 +33,10 @@ def setup_client(ip_address, port_number):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(connection_information)
 
+    return client
 
 # Send a message to the server.
-def send(message):
+def send(message, client):
     message = message.encode(FORMAT)                        # Encode the string in utf-8 format.
     msg_length = len(message)                           # Get the message length
     send_length = str(msg_length).encode(FORMAT)        # Make the send length using the msg length and the encode format.
@@ -44,34 +48,49 @@ def send(message):
     print(client.recv(2048).decode(FORMAT))
 
 
-def handle_message_from_server():
-    # create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # connect the socket to the server's address and port
-    server_address = (address, PORT)
-    sock.connect(server_address)
-
-    try:
-        # receive data from the server
-        data = sock.recv(1024)
-        return data.decode('utf-8')
-    finally:
-        # close the socket
-        sock.close()
+# def handle_message_from_server():
+#     # create a TCP/IP socket
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+#     # connect the socket to the server's address and port
+#     server_address = (address, PORT)
+#     sock.connect(server_address)
+#
+#     try:
+#         # receive data from the servera
+#         data = sock.recv(1024)
+#         return data.decode('utf-8')
+#     finally:
+#         # close the socket
+#         sock.close()
 
 
 # -------------------- RUNNING CODE --------------------------
 
-setup_client(address, PORT)
 
-game_running = True
+# ------------------------ CREATE CLIENT GUI STUFF ------------------------
 
-while game_running:
-    choice = input('Enter choice: ')
-    send(choice)
 
-    # print(handle_message_from_server())
+client = setup_client('10.14.0.2', 5050)
+send('message', client)
 
 
 
+# root = Tk()
+#
+# server_address_label = Label(root, text='Server Address')
+# server_address_textfield = Text(root, height=1, width=20)
+# connect_button = Button(root, text='Connect')
+# connection_status = Label(root, text='Disconnected')
+#
+# # rock_photo = tkinter.PhotoImage(file='assets/rock.jpg')
+# # rock_label = Label(root, image=rock_photo)
+#
+# server_address_label.grid(row=0, column=0)
+# server_address_textfield.grid(row=0, column=1)
+# connect_button.grid(row=1, column=0)
+# connection_status.grid(row=2, column=0)
+#
+# # rock_label.grid(row=3, column=0)
+#
+# root.mainloop()
--- rock_paper_scissors_server.py
+++ rock_paper_scissors_server.py
@@ -3,25 +3,43 @@
 
 import socket
 import threading
+from tkinter import *
 
 # -------------------------- SERVER-SIDE SETUP --------------------------
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())  # Will get the IP Address of this computer. If a VPN is used, will use that IP address instead.
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "exit"
-
-client_list = []
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
 
 
 
 # -------------------------- SERVER-SIDE --------------------------
 
 gameplay_list = []
+
+
+
+# -------------------------- SERVER SETUP --------------------------
+
+def start_server(SERVER, PORT):
+
+
+    server_running = True
+    client_list = []
+
+    # Instantiate the server.
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    server.bind((SERVER, PORT))
+
+    server.listen()
+    print(f"[LISTENING] SERVER IS LISTENING.{SERVER}")
+
+    while server_running:
+        conn, addr = server.accept()                                            # Accept any and all new client socket connections.
+        thread = threading.Thread(target=handle_client, args=(conn, addr))      # Pass the handle_client function to a thread so that the thread the can do it.
+        thread.start()                                                          # Start that thread.
+
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")           # Server-side debugging for printing the number of active socket connections.
+
+
 
 def handle_client(connection, address):
     print(f"[SERVER] NEW CONNECTION: {address} CONNECTED.")
@@ -32,19 +50,16 @@ def handle_client(connection, address):
 
     while server_running:
 
-        message_length = connection.recv(HEADER).decode(FORMAT)
-
-        if connection not in client_list:
-            client_list.append(connection)
+        message_length = connection.recv(64).decode('utf-8')
 
         if message_length:
             message_length = int(message_length)
-            message = connection.recv(message_length).decode(FORMAT)
+            message = connection.recv(message_length).decode('utf-8')
 
 
 
             # If statement that will deal with input data
-            if message == DISCONNECT_MESSAGE:       # If the server receives the disconnect message, disconnect the client.
+            if message == 'exit':       # If the server receives the disconnect message, disconnect the client.
                 server_running = False
             elif message == 'rock':
                 print('[SERVER] RECEIVED ROCK')
@@ -66,26 +81,59 @@ def handle_client(connection, address):
 
 
             print(f"[{address}] {message}")         # Debugging
-            connection.send(f"Message received: {message}".encode(FORMAT))
+            connection.send(f"Message received: {message}".encode('utf-8'))
 
-    connection.close()
-
-
-# -------------------------- SERVER SETUP --------------------------
-
-def start_server():
-    server.listen()
-    print(f"[LISTENING] SERVER IS LISTENING ON: {SERVER}")
-
-    while True:
-        conn, addr = server.accept()                                            # Accept any and all new client socket connections.
-        thread = threading.Thread(target=handle_client, args=(conn, addr))      # Instantiate a new thread for the new client.
-
-        thread.start()                                                          # Start that thread.
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")           # Debugging for printing the number of active socket connections.
+    connection.close_everything()
 
 
 
 # ------------------- RUNNING CODE -----------------------
 
-start_server()
+
+def start_start_server_thread(server_textarea):
+
+    HEADER = 64
+    PORT = 5050
+    SERVER = socket.gethostbyname(socket.gethostname())             # Will get the IP Address of this computer. If a VPN is used, will use that IP address instead.
+    ADDR = (SERVER, PORT)
+    FORMAT = 'utf-8'
+    DISCONNECT_MESSAGE = "exit"
+
+    # Run the server_thread method
+    server_thread = threading.Thread(target=start_server, args=(SERVER, PORT)).start()
+
+
+    # Run the handle_client method.
+    threading.Thread(target=handle_client, args=())
+    server_textarea.insert(INSERT, 'Server started.\n')
+
+
+
+# ------------------------ CREATE SERVER GUI STUFF ------------------------
+
+def close_everything():
+    root.destroy()
+
+
+# Create the server GUI
+
+root = Tk()
+root.title('Server GUI')
+
+# Instantiate components
+server_log_label = Label(root, text='Server Log')
+server_textarea = Text(root, height=20, width=40)
+
+start_server_button = Button(root, text='Start Server', command=lambda: start_start_server_thread(server_textarea))
+exit_program_button = Button(root, text='Exit', command=close_everything)
+
+# Place components on window
+server_log_label.grid(row=0, column=0)
+server_textarea.grid(row=1, column=0)
+start_server_button.grid(row=2, column=0)
+exit_program_button.grid(row=3, column=0)
+
+root.mainloop()
+
+
+
